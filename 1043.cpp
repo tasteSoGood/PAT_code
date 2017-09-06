92b007258b2914c71ac78f073b2c0b3b8e8824e2
tasteSoGood <970879461@qq.com>
2017-09-06 08:47:34 +0800

    taster

--- 1043.cpp
+++ 1043.cpp
@@ -1,36 +1,73 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-bool isBST(vector<int> arr, int a, int b){
+struct node {
+	int data;
+	node *left, *right;
+};
+bool is_tree(vector<int> arr, int a, int b, bool f){
 	if(a >= b)
 		return true;
 	int flag = arr[a], i;
-	for(i = a + 1; i < b && arr[i] < flag; i++);
-	for(int j = i; j < b; j++)
-		if(arr[j] < flag)
+	if(f)
+		for(i = a + 1; i < b && arr[i] < flag; i++);
+	else
+		for(i = a + 1; i < b && arr[i] >= flag; i++);
+	for(int j = i; j < b; j++){
+		if((f && arr[j] < flag) || (!f && arr[i] >= flag))
 			return false;
-	isBST(arr, a + 1, i);
-	isBST(arr, i, b);
+	}
+	is_tree(arr, a + 1, i, f);
+	is_tree(arr, i, b, f);
 }
-bool isMir(vector<int> arr, int a, int b){
-	if(a >= b)
-		return true;
-	int flag = arr[a], i;
-	for(i = a + 1; i < b && arr[i] >= flag; i++);
-	for(int j = i; j < b; j++)
-		if(arr[j] >= flag)
-			return false;
-	isMir(arr, a + 1, i);
-	isMir(arr, i, b);
+node *build_tree(vector<int> arr, int a, int b, bool flag){
+	if(a < b){
+		node *n = new node();
+		n->data = arr[a];
+		int i;
+		if(flag)
+			for(i = a + 1; i < b && arr[i] < arr[a]; i++);
+		else
+			for(i = a + 1; i < b && arr[i] >= arr[a]; i++);
+		n->right = build_tree(arr, a + 1, i, flag);
+		n->left = build_tree(arr, i, b, flag);
+		return n;
+	}
+	else
+		return NULL;
+}
+void postorder(node *t, vector<int> &po){
+	if(t->right) postorder(t->right, po);
+	if(t->left) postorder(t->left, po);
+	po.push_back(t->data);
+	delete t;
+}
+void print_arr(vector<int> arr){
+	for(int i = 0; i < arr.size(); i++){
+		if(i != 0)
+			cout << " ";
+		cout << arr[i];
+	}
+	cout << endl;
 }
 int main(){
 	int N;
 	cin >> N;
-	vector<int> preorder(N);
+	vector<int> preorder(N), po;
 	for(int i = 0; i < N; i++)
 		cin >> preorder[i];
-	if(isBST(preorder, 0, N) && isMir(preorder, 0, N))
+	if(is_tree(preorder, 0, N, true)){
 		cout << "YES" << endl;
+		node *tree = build_tree(preorder, 0, N, true);
+		postorder(tree, po);
+		print_arr(po);
+	}
+	else if(is_tree(preorder, 0, N, false)){
+		cout << "YES" << endl;
+		node *tree = build_tree(preorder, 0, N, false);
+		postorder(tree, po);
+		print_arr(po);
+	}
 	else
 		cout << "NO" << endl;
 	return 0;
